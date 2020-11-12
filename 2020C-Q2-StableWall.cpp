320ecc4d94207daa396ea9c9ffa0ea68afcb2714
Brodong <757264690@qq.com>
2020-11-13 01:43:41 +0800

    WA-拓扑排序

--- 2020C-Q2-StableWall.cpp
+++ 2020C-Q2-StableWall.cpp
@@ -5,7 +5,7 @@
 #        Author: Li Xudong - 757264690@qq.com
 #   Description: ---
 #        Create: 2020-11-12 15:08:39
-# Last Modified: 2020-11-12 15:08:39
+# Last Modified: 2020-11-13 01:42:42
 #
  ***********************************************/
 #include <iostream>
@@ -13,6 +13,7 @@
 #include <stdlib.h>
 #include <vector>
 #include <queue>
+#include <string.h>
 using namespace std;
 
 // 这一题其实很简单
@@ -31,7 +32,7 @@ using namespace std;
 // 对于第二列,从往上,分别是M-F-M,则需要在顶点M和顶点F之间增加一条边,之后在顶点F和顶点M之间增加一条边(其实,这时候已经有环了,是不稳定的)
 // 建好拓扑图后,拓扑排序,输入节点即可,如果没有将所有节点输出,则有环,输出-1
 //
-#define maxN 30
+#define maxN 30+5
 // 26个顶点
 #define maxP 26
 
@@ -45,7 +46,7 @@ int main()
     int cnt; // 统计字母个数
     scanf("%d", &T);
     // 拓扑图
-    vector<vector<int>> G[maxP];
+    vector< vector<int> > G(maxP);
     // 入度
     int indegree[maxP];
     // 拓扑排序的输出队列
@@ -57,11 +58,12 @@ int main()
         // 有一些多余的字母可能没有用,因此用-1标记
         memset(indegree, -1, sizeof(indegree));
         for(i=0; i<R; i++)
-            for(j=0; j<R; j++)
+            for(j=0; j<C; j++)
             { 
-                scanf("%c", &alp[i][j]);
+                scanf(" %c", &alp[i][j]);
                 indegree[alp[i][j]-'A'] = 0;
             }
+    
         // 统计字母个数
         cnt = 0;
         for(i=0; i<maxP; i++)
@@ -73,27 +75,27 @@ int main()
             for(j=R-1; j>0; j--)
             {
                 // 如果上下两个字母相同,则不添加任何边
-                if(alp[i][j] == alp[i][j-1])
+                if(alp[j][i] == alp[j-1][i])
                 {
-
                 }
                 else
                 {
                     //如果不相同,需要查看是否有这个边,重复的边只保留一次
-                    for(k=0; k<G[alp[i][j]-'A'].size; k++)
+                    for(k=0; k<G[alp[j][i]-'A'].size(); k++)
                     {
-                        if(G[alp[i][j]-'A'][k] == alp[i][j-1]-'A')
+                        if(G[alp[j][i]-'A'][k] == alp[j-1][i]-'A')
                             break;
                     }
-                    if(k == G[alp[i][j]-'A'].size)
+                    if(k == G[alp[j][i]-'A'].size())
                     {
-                        G[alp[i][j]-'A'].push(alp[i][j-1]-'A');
+                        G[alp[j][i]-'A'].push_back(alp[j-1][i]-'A');
                         // 入度加1            
-                        indegree[alp[i][j-1]-'A']++;
+                        indegree[alp[j-1][i]-'A']++;
                     }
                 }
             }
         }
+
 
         // 建好图后, 就可以拓扑排序了
         // 先添加入度为0的序列入队
@@ -108,13 +110,13 @@ int main()
         while(!topsort.empty())
         {
             // 输出入度为0的顶点,由于不知道有没有环,还不呢输出
-            //printf("%c", topsort.top()+'A');
-            k = topsort.top();
+            // printf("%c", topsort.front()+'A');
+            k = topsort.front();
             topsort2.push(k);
             topsort.pop();
             j++;
             // 删除该顶点出发的边,并且对应的顶点入度-1
-            for(i=0; i<G[k].size; i++)
+            for(i=0; i<G[k].size(); i++)
             {
                 indegree[G[k][i]]--;
                 // 如果为0,则加入队列
@@ -124,16 +126,25 @@ int main()
 
         }
 
+        // 记得删除所有的点
+        for(i=0; i<maxP; i++)
+        {
+            G[i].clear();
+        }
+
+        printf("Case #%d: ",t);
         if(j == cnt)
         {
             while(!topsort2.empty())
             {
-
+                printf("%c", topsort2.front()+'A');
+                topsort2.pop();
             }
+            printf("\n");
         }
         else
             printf("-1\n");
-        printf("Case #%d: ",t);
+        /* printf("Case #%d: ",t); */
         
     }
     /* std::cout << "Hello world" << std::endl; */
